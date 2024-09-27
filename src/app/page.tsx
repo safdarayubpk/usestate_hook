137679df084d8fd03b93dc28aab92642eeb22ab8
safdarayub <safdarayub@gmail.com>
2024-09-27 21:57:51 +0500

    Initial commit from Create Next App

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Homepage() {
+  return (
+    <div>
+        <div className='bg-red-400'>Content 1</div>
+        <div className='bg-green-400'>Content 2</div>
+        <div className='bg-blue-400'>Content 3</div>
+    </div>
+  )
+}
